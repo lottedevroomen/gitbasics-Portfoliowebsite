1c7a21fb91bdf233807596ce3d2f6478b7f69806
lottedevroomen <47530070+lottedevroomen@users.noreply.github.com>
2019-03-12 20:34:54 +0100

    new

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
-TweenMax.to(".intro", 2, {scaleY: 1,  delay:2}); 
+
+var myIndex = 0;
+carouseleen();
+
+function carouseleen() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carouseleen, 3000); 
+}
+
+
+var myIndex = 0;
+carouseltwee();
+
+function carouseltwee() {
+  var i;
+  var x = document.getElementsByClassName("Slides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carouseltwee, 3000); 
+}
